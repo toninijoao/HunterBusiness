581bb857ff19eace3734f3c417f9781a3c248371
toninijoao <joaopansanatotonini@gmail.com>
2026-09-03 19:16:56 -0300

    adicionando codigo de main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from orquestrador.orquestrador import carregar_config, criar_tarefa, executar_pipeline
+
+def main():
+    config = carregar_config()
+
+    tarefa = criar_tarefa(config)
+
+    resultado = executar_pipeline(tarefa)
+
+    print(
+        f"\nPipeline: concluído."
+        f"\nEmpresas encontradas: {resultado['quantidade_encontrada']}"
+        f"\nEmpresas processadas: {resultado['quantidade_processada']}"
+    )
+
+if __name__ == "__main__":
+    main()
